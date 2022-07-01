5d4e0fb5fd95c28adaab57b94aa487e1d9151af9
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-01 09:49:33 -0700

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,10 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch == 'e' || ch == 'q') continue;
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		}
 		putchar(ch);
 	}
 	putchar('\n');
