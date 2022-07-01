4866aaf84f098eb1dbe220b7f5615cacc9f5de4b
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-01 09:25:34 -0700

    fix

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * Print alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
