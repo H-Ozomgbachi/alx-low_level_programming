7d10b1894bb22831b16d7c0f1a124e964689abe1
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-05 15:50:33 -0700

    fix

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - check if text is lower
+ * @n: integer param
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}
