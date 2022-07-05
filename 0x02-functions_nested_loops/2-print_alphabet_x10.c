168d101b8244a2a2c2b77b63b3f1d23d6ee156d9
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-05 12:58:26 -0700

    fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints letters of English alphabets 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+
+	_putchar('\n');
+}
