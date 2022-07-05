465a372cb2781c42dc95d8d9664d53e9f50a47d3
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-05 15:01:18 -0700

    fix

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - check if text is alpha
+ * @c: a character params
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	int isalpha = 0;
+
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		{
+			isalpha = 1;
+			break;
+		}
+	}
+	
+	for (j = 'A'; J <= 'Z'; j++)
+	{
+		if (j == c)
+		{
+			isalpha = 1;
+			break;
+		}
+	}
+
+	return (isalpha);
+}
