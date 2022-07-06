b2a619fc1eb46cfccd9b1c04b6d8aeb2a3242a06
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-05 22:47:04 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,33 @@
 void print_to_98(int n)
 {
 	if (n <= 98)
+	{
 		int x = n;
 
 		while (x < 99)
+		{
 			printf("%d", x);
 			if (x == 98)
+			{
 				break;
+			}
 			_putchar(',');
 			_putchar(' ');
 			x++;
+		}
+	}
 	else
+	{
 		int y = n;
 
 		while (y >= 98)
+		{
 			printf("%d", y);
 			if (y == 98)
 				break;
 			_putchar(',');
 			_putchar(' ');
 			y--;
+		}
+	}
 }
