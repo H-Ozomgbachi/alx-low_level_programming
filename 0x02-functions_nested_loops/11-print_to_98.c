9a1106d19b48c013e1bc4c71a5887ba78e50d08d
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-05 22:31:51 -0700

    fix

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @n: value
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+		int x = n;
+
+		while (x < 99)
+			printf("%d", x);
+			if (x == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			x++;
+	else
+		int y = n;
+
+		while (y >= 98)
+			printf("%d", y);
+			if (y == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			y--;
+}
