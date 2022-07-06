9ac63b88787f4377bda55ef63ed3d0220e2886c0
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-05 22:05:28 -0700

    fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two nums
+ * @x: first value
+ * @y: second value
+ * Return: zsero on success
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
