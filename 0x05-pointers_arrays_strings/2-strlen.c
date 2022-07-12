cbcf138dfdc1e5ed7d14e9667404f15c9fc5b02c
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-12 08:56:01 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - print length of string
+ * @s: param to print
+ * Return: an integer of the length of s
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
