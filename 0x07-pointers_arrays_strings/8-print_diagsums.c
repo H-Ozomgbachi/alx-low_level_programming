1020b48605efd827767602512178ab37cc74ae5c
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-18 17:55:04 -0700

    fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,13 +12,11 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0, sum2 = 0;
 	int i, max = size * size;
 
-	for (i = 0; i < max; i++)
-	{
-		if (i % (size + 1) == 0)
-			sum1 += a[i];
-		if (i % (size - 1) == 0 && i <= (size) * (size - 1))
-			sum2 += a[i];
-	}
+	for (i = 0; i < max; i += size + 1)
+		sum1 += a[i];
+
+	for (i = size - 1; i < max - 1; i += size - 1)
+		sum2 += a[i];
 
 	printf("%d, %d\n", sum1, sum2);
 }
