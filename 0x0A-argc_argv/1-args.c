3d150af3c7fae1a8a745c8eac81ad56c4bef765b
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-22 13:49:42 -0700

    fix

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: zero on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = argc - 1;
+
+	printf("%d\n", i);
+
+	return (0);
+}
