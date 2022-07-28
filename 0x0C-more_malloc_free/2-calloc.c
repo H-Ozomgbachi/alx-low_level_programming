3cdcba19d06275565f763318ebed5c3a19e3ce4c
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-07-28 14:27:32 -0700

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *s;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	s = malloc(nmemb * size);
+
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
 		s[i] = 0;
