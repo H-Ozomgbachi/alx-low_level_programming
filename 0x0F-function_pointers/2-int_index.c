0d9ea2628dfd6d3204d114b471deb49baf4d6f79
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-08-03 14:26:45 -0700

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,12 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, res = -1;
 
-	if (!(array) || !(cmp) || size <= 0)
-		return (-1);
+	if (!(array) || !(cmp))
+		exit(1);
+	if (size <= 0)
+		return (res);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i] != 0))
+		if (cmp(array[i]) != 0)
 		{
 			res = i;
 			break;
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,5 +9,8 @@ void print_name_uppercase(char *name);
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
 
 #endif
