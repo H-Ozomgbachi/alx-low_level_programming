d5b6af125440d8629b1cb70b1f193438c14e8d5a
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-08-03 10:50:13 -0700

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (!(array) || !(action))
+	if (!(array) || !(action) || !(size))
 		return;
 
 	for (i = 0; i < size; i++)
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
 
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
