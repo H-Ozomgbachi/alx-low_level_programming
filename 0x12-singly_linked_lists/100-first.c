cb6cb90423fbfa6200f2fab1717cdbdc2144934f
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-08-12 08:04:48 -0700

    fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
-#include "lists.h"
+#include <stdio.h>
 
 /**
  * before_main - execute before the main
  */
+void before_main(void) __attribute__((constructor));
+
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
