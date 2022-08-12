fec68644dd30700c86281d3dc48efd6f28ccfbe5
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-08-12 06:13:40 -0700

    fix

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - add node
+ * @head: reference to head node
+ * @str: new node
+ * Return: address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int len = 0;
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	new_node->str = str;
+	new_node->len = len;
+	new_node->next = (*head);
+
+	(*head) = new_node;
+
+	return (new_node);
+}
