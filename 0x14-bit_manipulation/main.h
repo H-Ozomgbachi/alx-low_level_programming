872f24dbe183c1774434eccdef1d2a49a1fe0054
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-08-19 13:53:42 -0700

    fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ void _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+unsigned long int _pow(unsigned int base, unsigned int power);
 
 #endif
