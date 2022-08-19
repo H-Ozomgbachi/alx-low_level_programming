b29778527d2e09898613354d7622b8514259c353
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-08-19 14:23:20 -0700

    fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ int get_bit(unsigned long int n, unsigned int index);
 unsigned long int _pow(unsigned int base, unsigned int power);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
